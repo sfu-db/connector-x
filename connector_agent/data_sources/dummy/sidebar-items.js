5ede4d051f651e2a89675670cb8595aed13e7590
wangxiaoying <wangxiaoying0369@gmail.com>
2021-01-26 22:28:02 +0000

    Deploying to gh-pages from  @ 7832ce58376c102b9d7be750a6ad1babae4a14b8 🚀

--- connector_agent/data_sources/dummy/sidebar-items.js
+++ connector_agent/data_sources/dummy/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["StringSource",""],["U64CounterSource","This `DataSource` only produces T which can be derived from u64."]]});
+initSidebarItems({"struct":[["BoolCounterSource","This `DataSource` only produces T which can be derived from bool."],["StringSource",""],["U64CounterSource","This `DataSource` only produces T which can be derived from u64."]]});
