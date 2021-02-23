4233f41e8fbbc3f23f4c24b225358460579cd6c2
Weiyuan Wu <youngw@sfu.ca>
2021-02-23 05:34:14 +0000

    add some checks

--- connector-agent/src/errors.rs
+++ connector-agent/src/errors.rs
@@ -19,9 +19,15 @@ pub enum ConnectorAgentError {
     #[error("Cannot resolve data order: got {0:?} from source, {1:?} from destination.")]
     CannotResolveDataOrder(Vec<DataOrder>, Vec<DataOrder>),
 
-    #[error("Unexpected value, expect t or f, found {0}")]
+    #[error("Unexpected value, expect t or f, found {0}.")]
     CannotParsePostgresBool(String),
 
+    #[error("Allocate is already called.")]
+    DuplicatedAllocation,
+
+    #[error("Writer has not been allocated yet.")]
+    WriterNotAllocated,
+    
     #[error(transparent)]
     IOError(#[from] std::io::Error),
 
