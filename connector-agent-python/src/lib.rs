19167aa531b21018330953bbefe288585df3a4de
Weiyuan Wu <youngw@sfu.ca>
2021-02-23 18:17:16 +0000

    add skeleton

--- connector-agent-python/src/lib.rs
+++ connector-agent-python/src/lib.rs
@@ -19,6 +19,8 @@ fn connector_agent_python(_: Python, m: &PyModule) -> PyResult<()> {
     // https://github.com/PyO3/pyo3-built/issues/21
     // m.add("__build__", pyo3_built!(py, build))?;
     m.add_wrapped(wrap_pyfunction!(write_pandas))?;
+    m.add_wrapped(wrap_pyfunction!(read_sql))?;
+
     Ok(())
 }
 
@@ -30,4 +32,22 @@ fn write_pandas<'a>(
     schema: Vec<&str>,
 ) -> PyResult<&'a PyAny> {
     Ok(crate::pandas::write_pandas(py, conn, &queries, &schema)?)
+}
+
+#[pyfunction]
+fn read_sql<'a>(
+    py: Python<'a>,
+    conn: &str,
+    query: &str,
+    schema: Vec<&str>, // TODO: remove this once inferschema is done
+    return_type: &str,
+) -> PyResult<&'a PyAny> {
+    todo!("split the query");
+    let queries = vec![];
+
+    match return_type {
+        "pandas" => Ok(crate::pandas::write_pandas(py, conn, &queries, &schema)?),
+        "arrow" => todo!(),
+        _ => todo!("Error handling"),
+    }
 }
