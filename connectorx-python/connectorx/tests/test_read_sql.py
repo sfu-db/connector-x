c6ed1616ba91d2e01637ad99779da84220c9f8bf
Wukkkinz-0725 <jinzew@sfu.ca>
2021-04-07 04:55:49 -0700

    add more test cases

--- connectorx-python/connectorx/tests/test_read_sql.py
+++ connectorx-python/connectorx/tests/test_read_sql.py
@@ -137,6 +137,50 @@ def test_read_sql_with_partition_and_selection(postgres_url: str) -> None:
     assert_frame_equal(df, expected, check_names=True)
 
 
+def test_read_sql_with_partition_and_projection(postgres_url: str) -> None:
+    query = "SELECT test_int, test_float, test_str FROM test_table"
+    df = read_sql(
+        postgres_url,
+        query,
+        partition_on="test_int",
+        partition_range=(0, 2000),
+        partition_num=3,
+    )
+    expected = pd.DataFrame(
+        index=range(6),
+        data={
+            "test_int": pd.Series([1, 2, 0, 3, 4, 1314], dtype="Int64"),
+            "test_float": pd.Series([None, 2.2, 3.1, 3, 7.8, -10], dtype="float64"),
+            "test_str": pd.Series(
+                ["str1", "str2", "a", "b", "c", None], dtype="object"
+            ),
+        },
+    )
+    assert_frame_equal(df, expected, check_names=True)
+
+
+def test_read_sql_with_partition_and_join(postgres_url: str) -> None:
+    query = "SELECT T.test_int, T.test_bool, S.test_language FROM test_table T INNER JOIN test_str S ON T.test_int = S.id"
+    df = read_sql(
+        postgres_url,
+        query,
+        partition_on="test_int",
+        partition_range=(0, 2000),
+        partition_num=3,
+    )
+    expected = pd.DataFrame(
+        index=range(5),
+        data={
+            "test_int": pd.Series([0, 1, 2, 3, 4], dtype="Int64"),
+            "test_bool": pd.Series([None, True, False, False, None], dtype="boolean"),
+            "test_language": pd.Series(
+                ["English", "中文", "日本語", "русский", "Emoji"], dtype="object"
+            ),
+        },
+    )
+    assert_frame_equal(df, expected, check_names=True)
+
+
 def test_read_sql_on_utf8(postgres_url: str) -> None:
     query = "SELECT * FROM test_str"
     df = read_sql(postgres_url, query)
