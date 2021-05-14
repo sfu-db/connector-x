70a8e5d6bff5f0d77d5042604c4507ea04141d96
wangxiaoying <wangxiaoying0369@gmail.com>
2021-05-14 21:27:16 +0000

    update sqlx with runtime

--- connectorx/examples/sqlx_rt.rs
+++ connectorx/examples/sqlx_rt.rs
@@ -8,11 +8,11 @@ use tokio::runtime;
 // #[async_std::main]
 fn main() -> Result<(), sqlx::Error> {
     // build runtime
-    let runtime = runtime::Builder::new_current_thread()
+    let rt = runtime::Builder::new_current_thread()
         .enable_all()
         .build()?;
 
-    let pool = runtime.block_on(async {
+    let pool = rt.block_on(async {
         let pool = PgPoolOptions::new()
             .max_connections(5)
             .connect("postgres://postgres:postgres@localhost:5432/tpch")
@@ -20,17 +20,21 @@ fn main() -> Result<(), sqlx::Error> {
         Ok::<_, sqlx::Error>(pool)
     })?;
 
-    let count = 10;
+    let count = 100000;
 
     println!("run fetch");
     let query = format!("SELECT * FROM lineitem limit {}", count);
-    let mut rows = sqlx::query(query.as_str()).fetch(&pool);
+
+    let mut rows = rt.block_on(async {
+        let rows = sqlx::query(query.as_str()).fetch(&pool);
+        Ok::<_, sqlx::Error>(rows)
+    })?;
 
     let start_stmp = Instant::now();
     println!("start traverse!");
 
     for _i in 0..count {
-        let row = runtime.block_on(async {
+        let row = rt.block_on(async {
             let row = rows.try_next().await?.expect("no more rows!");
             Ok::<_, sqlx::Error>(row)
         })?;
@@ -52,27 +56,30 @@ fn main() -> Result<(), sqlx::Error> {
         let _l_shipmode: &str = row.try_get(14)?;
         let _l_comment: &str = row.try_get(15)?;
 
-        println!(
-            "{},{},{},{},{},{},{},{},{},{},{:?},{:?},{:?},{},{},{}",
-            _l_orderkey,
-            _l_partkey,
-            _l_suppkey,
-            _l_linenumber,
-            _l_quantity,
-            _l_extendedprice,
-            _l_discount,
-            _l_tax,
-            _l_returnflag,
-            _l_linestatus,
-            _l_shipdate,
-            _l_commitdate,
-            _l_receiptdate,
-            _l_shipinstruct,
-            _l_shipmode,
-            _l_comment,
-        );
+        // println!(
+        //     "{},{},{},{},{},{},{},{},{},{},{:?},{:?},{:?},{},{},{}",
+        //     _l_orderkey,
+        //     _l_partkey,
+        //     _l_suppkey,
+        //     _l_linenumber,
+        //     _l_quantity,
+        //     _l_extendedprice,
+        //     _l_discount,
+        //     _l_tax,
+        //     _l_returnflag,
+        //     _l_linestatus,
+        //     _l_shipdate,
+        //     _l_commitdate,
+        //     _l_receiptdate,
+        //     _l_shipinstruct,
+        //     _l_shipmode,
+        //     _l_comment,
+        // );
     }
     println!("Traverse finished, took {:?}", start_stmp.elapsed());
+
+    // before exiting current block, destruct the rows in an async runtime
+    rt.block_on(async move { drop(rows) });
 
     Ok(())
 }
