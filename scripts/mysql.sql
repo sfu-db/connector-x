17221e6d620da7d5cd33f81092d125ad10d37150
Yizhou <yizhoum150@gmail.com>
2021-06-01 21:50:42 +0800

    mysql.sql

--- scripts/mysql.sql
+++ scripts/mysql.sql
@@ -1,9 +1,9 @@
 DROP TABLE IF EXISTS test_table;
 
 CREATE TABLE IF NOT EXISTS test_table(
-    test_int INTEGER NOT NULL,
-    test_float DOUBLE PRECISION,
-    test_str TEXT,
+    test_int INTEGER,
+    test_float DOUBLE,
+    test_str TEXT
 );
 
 INSERT INTO test_table VALUES (1, 1.1, 'str1');
