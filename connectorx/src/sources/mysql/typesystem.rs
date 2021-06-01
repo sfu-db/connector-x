4c589413bef540d1b535684e83bf29d47467bc26
Yizhou <yizhoum150@gmail.com>
2021-06-01 21:26:25 +0800

    ts6.1

--- connectorx/src/sources/mysql/typesystem.rs
+++ connectorx/src/sources/mysql/typesystem.rs
@@ -9,7 +9,6 @@ use uuid::Uuid;
 pub enum MysqlTypeSystem {
     Double(bool),
     Long(bool),
-    Text(bool),
 }
 
 impl_typesystem! {
@@ -17,33 +16,27 @@ impl_typesystem! {
     mappings = {
         { Long => i64 }
         { Double => f64 }
-        { Text => &'r str }
     }
 }
 
 impl<'a> From<&'a ColumnType> for MysqlTypeSystem {
     fn from(ty: &'a ColumnType) -> MysqlTypeSystem {
         use MysqlTypeSystem::*;
-        match ty.name() {
-            "MYSQL_TYPE_LONG" => Long(true),
-            "MYSQL_TYPE_DOUBLE" => Double(true),
-            "MYSQL_TYPE_" => Text(true),
-            _ => match ty.kind() {
-                postgres::types::Kind::Enum(_) => Enum(true),
-                _ => unimplemented!("{}", ty.name()),
-            },
+        match ty {
+            ColumnType::MYSQL_TYPE_LONG => Long(true),
+            ColumnType::MYSQL_TYPE_DOUBLE => Double(true),
+            _ => unimplemented!("{}", ty.name()),
         }
     }
 }
 
-// Link MysqlDTypes back to the one defiend by the postgres crate.
+// Link MysqlDTypes back to the one defined by the mysql crate.
 impl<'a> From<MysqlTypeSystem> for ColumnType {
     fn from(ty: MysqlTypeSystem) -> ColumnType {
         use MysqlTypeSystem::*;
         match ty {
-            Int4(_) => Type::INT4,
-            Float4(_) => Type::FLOAT4,
-            Text(_) => Type::TEXT,
+            Long(_) => ColumnType::MYSQL_TYPE_LONG,
+            Double(_) => ColumnType::MYSQL_TYPE_DOUBLE,
         }
     }
 }
