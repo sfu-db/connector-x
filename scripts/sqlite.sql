90bda5639dd17cb933bb02ba23685b0681121d12
wangxiaoying <wangxiaoying0369@gmail.com>
2021-06-11 06:30:18 +0000

    update sqlite script file

--- scripts/sqlite.sql
+++ scripts/sqlite.sql
@@ -1,16 +1,17 @@
 DROP TABLE IF EXISTS test_table;
 
-CREATE TABLE test_table(
+CREATE TABLE IF NOT EXISTS test_table(
     test_int INTEGER NOT NULL,
     test_nullint INTEGER,
     test_str TEXT,
-    test_float REAL
+    test_float REAL,
+    test_bool BOOLEAN,
+    test_date DATE
 );
 
-INSERT INTO test_table VALUES (1, 3, 'str1', NULL);
-INSERT INTO test_table VALUES (2, NULL, 'str2', 2.2);
-INSERT INTO test_table VALUES (0, 5, 'a', 3.1);
-INSERT INTO test_table VALUES (3, 7, 'b', 3);
-INSERT INTO test_table VALUES (4, 9, 'c', 7.8);
-INSERT INTO test_table VALUES (1314, 2, NULL, -10);
-
+INSERT INTO test_table VALUES (1, 3, 'str1', NULL, True, '1996-03-13');
+INSERT INTO test_table VALUES (2, NULL, 'str2', 2.2, False, '1996-01-30');
+INSERT INTO test_table VALUES (0, 5, 'こんにちは', 3.1, NULL, '1996-02-28');
+INSERT INTO test_table VALUES (3, 7, 'b', 3, False, '2020-01-12');
+INSERT INTO test_table VALUES (4, 9, 'Ha好ち😁ðy̆', 7.8, NULL, '1996-04-20');
+INSERT INTO test_table VALUES (1314, 2, NULL, -10, True, NULL);
