c8f3682808e0706d8ca71c04af680d1a3511e255
github-action-benchmark <github@users.noreply.github.com>
2021-07-20 03:03:27 +0000

    add ConnectorX TPCH Scale1 Benchmarks (pytest) benchmark result for 64ca37e94fd51fc63bca337b1f74214b88e76f89

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1626729742302,
+  "lastUpdate": 1626750207747,
   "repoUrl": "https://github.com/sfu-db/connector-x",
   "entries": {
     "ConnectorX Benchmarks": [
@@ -38,6 +38,46 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 2.597003040953377",
             "extra": "mean: 16.475381082785315 sec\nrounds: 5"
+          }
+        ]
+      }
+    ],
+    "ConnectorX TPCH Scale1 Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "youngw@sfu.ca",
+            "name": "Weiyuan Wu",
+            "username": "dovahcrow"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "64ca37e94fd51fc63bca337b1f74214b88e76f89",
+          "message": "Merge pull request #100 from sfu-db/feat/benchmark_bot\n\nFeat/benchmark bot",
+          "timestamp": "2021-07-19T19:54:53-07:00",
+          "tree_id": "36eeff939bec1bbc5584b3a039dfab46b4e7e48d",
+          "url": "https://github.com/sfu-db/connector-x/commit/64ca37e94fd51fc63bca337b1f74214b88e76f89"
+        },
+        "date": 1626750206747,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "connectorx/tests/benchmarks.py::bench_mysql",
+            "value": 0.0608387087642097,
+            "unit": "iter/sec",
+            "range": "stddev: 0.6522933649056051",
+            "extra": "mean: 16.436903746193273 sec\nrounds: 5"
+          },
+          {
+            "name": "connectorx/tests/benchmarks.py::bench_postgres",
+            "value": 0.05540078267880269,
+            "unit": "iter/sec",
+            "range": "stddev: 2.776374003088519",
+            "extra": "mean: 18.050286505836993 sec\nrounds: 5"
           }
         ]
       }
