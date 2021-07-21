7250021f69a78efd6b98694cfaf11dbf776d67e9
Xiaoying Wang <xiaoying_wang@sfu.ca>
2021-07-21 20:38:57 +0000

    benchmark for dask and modin, support mysql and sqlite

--- benchmarks/tpch-dask.py
+++ benchmarks/tpch-dask.py
@@ -1,8 +1,10 @@
 """
 Usage:
-  tpch-dask.py <num>
+  tpch-dask.py <num> [--conn=<conn>] [--index=<idx>]
 
 Options:
+  --conn=<conn>          The connection url to use [default: POSTGRES_URL].
+  --index=<idx>          The connection url to use [default: l_orderkey].
   -h --help     Show this screen.
   --version     Show version.
 """
@@ -16,17 +18,23 @@ from dask.distributed import Client, LocalCluster
 
 if __name__ == "__main__":
     args = docopt(__doc__, version="Naval Fate 2.0")
-    conn = os.environ["POSTGRES_URL"]
-    table = os.environ["POSTGRES_TABLE"]
+    index_col = args["--index"]
+    conn = os.environ[args["--conn"]]
+    table = os.environ["TPCH_TABLE"]
     npartition = int(args["<num>"])
     cluster = LocalCluster(n_workers=npartition, scheduler_port=0, memory_limit="230G")
     client = Client(cluster)
+
+    # https://docs.sqlalchemy.org/en/13/core/engines.html#sqlite
+    # 4 initial slashes is needed for Unix/Mac
+    if conn.startswith("sqlite"):
+      conn = f"sqlite:///{conn[9:]}"
 
     with Timer() as timer:
         df = dd.read_sql_table(
             table,
             conn,
-            "l_orderkey",
+            index_col,
             npartitions=npartition,
             limits=(0, 60000000),
         ).compute()
