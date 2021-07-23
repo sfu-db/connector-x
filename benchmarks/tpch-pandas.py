d87efa1ae7c3c5dff079e425d6178a902f3bac0e
Xiaoying Wang <xiaoying_wang@sfu.ca>
2021-07-23 01:18:55 +0000

    [skip ci] update benchmark script for pandas for more drivers

--- benchmarks/tpch-pandas.py
+++ benchmarks/tpch-pandas.py
@@ -1,11 +1,18 @@
 """
 Usage:
-  tpch-pandas.py [--conn=<conn>]
+  tpch-pandas.py [--conn=<conn>] [--driver=<driver>]
 
 Options:
-  --conn=<conn>          The connection url to use [default: POSTGRES_URL].
-  -h --help     Show this screen.
-  --version     Show version.
+  --conn=<conn>             The connection url to use [default: POSTGRES_URL].
+  --driver=<driver>         The driver to use using sqlalchemy: https://docs.sqlalchemy.org/en/14/core/engines.html.
+  -h --help                 Show this screen.
+  --version                 Show version.
+
+Drivers:
+  PostgreSQL: postgresql, postgresql+psycopg2
+  MySQL: mysql, mysql+mysqldb, mysql+pymysql
+  Redshift: postgresql, redshift, redshift+psycopg2
+
 """
 
 import os
@@ -16,22 +23,25 @@ from docopt import docopt
 import pandas as pd
 import sqlite3
 from clickhouse_driver import connect
+from sqlalchemy.engine.url import make_url
 
 if __name__ == "__main__":
     args = docopt(__doc__, version="1.0")
-    conn = os.environ[args["--conn"]]
     table = os.environ["TPCH_TABLE"]
+    driver = args.get("--driver", None)
+    conn = os.environ[args["--conn"]]
+    conn = make_url(conn)
 
-    if conn.startswith("sqlite"):
-        conn = sqlite3.connect(conn[9:])
-    elif conn.startswith("mysql"):
-        if args["--conn"] == "CLICKHOUSE_URL":
-            # clickhouse-driver uses native protocol: 9000
-            conn = connect(f"clickhouse://{os.environ['CLICKHOUSE_USER']}:{os.environ['CLICKHOUSE_PASSWORD']}@{os.environ['CLICKHOUSE_HOST']}:9000/tpch") 
-        else:
-            engine = create_engine(f"mysql+mysqldb:{conn[6:]}")
-            conn = engine.connect()
-    else:
+    if conn.drivername == "sqlite":
+        conn = sqlite3.connect(str(conn)[9:])
+    elif driver == "clickhouse":
+        # clickhouse-driver uses native protocol: 9000
+        conn = conn.set(drivername=driver, port=9000)
+        conn = connect(str(conn))
+    else:  # go with sqlalchemy
+        if driver is not None:
+            conn = conn.set(drivername=driver)
+        print(f"conn url: {str(conn)}")
         engine = create_engine(conn)
         conn = engine.connect()
 
