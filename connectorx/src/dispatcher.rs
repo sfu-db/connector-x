8d2f05e955d270b7804a0fbb19520505b87b2d63
Weiyuan Wu <youngw@sfu.ca>
2021-07-29 19:10:50 +0000

    fix docstring

--- connectorx/src/dispatcher.rs
+++ connectorx/src/dispatcher.rs
@@ -1,6 +1,5 @@
-///! This module provides [`dispatcher::Dispatcher`], the core struct in ConnectorX 
-//! that drives the data loading from a source to a destination.
-
+///! This module provides [`dispatcher::Dispatcher`], the core struct in ConnectorX
+///! that drives the data loading from a source to a destination.
 use crate::{
     data_order::{coordinate, DataOrder},
     destinations::{Destination, DestinationPartition},
