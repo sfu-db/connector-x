d33ec4964915012a234ddc6c41d5e3f7ea84e585
Wukkkinz-0725 <jinzew@sfu.ca>
2021-09-28 14:31:37 +0000

    improved test cases for mysql postgres sqlite

--- connectorx-python/connectorx/tests/test_sqlite.py
+++ connectorx-python/connectorx/tests/test_sqlite.py
@@ -104,6 +104,94 @@ def test_read_sql_with_partition(sqlite_db: str) -> None:
     )
     assert_frame_equal(df, expected, check_names=True)
 
+def test_manual_partition(sqlite_db: str) -> None:
+
+    queries = [
+        "SELECT test_int, test_nullint, test_str, test_float, test_bool, test_date, test_time, test_datetime FROM test_table WHERE test_int < 2",
+        "SELECT test_int, test_nullint, test_str, test_float, test_bool, test_date, test_time, test_datetime FROM test_table WHERE test_int >= 2",
+    ]
+
+    df = read_sql(sqlite_db, query=queries)
+
+    expected = pd.DataFrame(
+        index=range(6),
+        data={
+            "test_int": pd.Series([1, 0, 2, 3, 4, 1314], dtype="Int64"),
+            "test_nullint": pd.Series([3, 5, None, 7, 9, 2], dtype="Int64"),
+            "test_str": pd.Series(
+                ["str1", "こんにちは", "str2", "b", "Ha好ち😁ðy̆", None], dtype="object"
+            ),
+            "test_float": pd.Series([None, 3.1, 2.2, 3, 7.8, -10], dtype="float64"),
+            "test_bool": pd.Series(
+                [True, None, False, False, None, True], dtype="boolean"
+            ),
+            "test_date": pd.Series(
+                [
+                    np.datetime64("1996-03-13"),
+                    np.datetime64("1996-02-28"),
+                    np.datetime64("1996-01-30"),
+                    np.datetime64("2020-01-12"),
+                    np.datetime64("1996-04-20"),
+                    None
+                ], dtype="datetime64[ns]"
+            ),
+            "test_time": pd.Series(
+                ["08:12:40", "23:00:10", "10:03:00", "23:00:10", "18:30:00", "18:30:00"], dtype="object"
+            ),
+            "test_datetime": pd.Series(
+                [
+                    np.datetime64("2007-01-01T10:00:19"),
+                    None,
+                    np.datetime64("2005-01-01T22:03:00"),
+                    np.datetime64("1987-01-01T11:00:00"),
+                    None,
+                    np.datetime64("2007-10-01T10:32:00")
+                ], dtype="datetime64[ns]"
+            ),
+        },
+    )
+    assert_frame_equal(df, expected, check_names=True)
+
+def test_read_sql_without_partition_and_spa(sqlite_db: str) -> None:
+    query = """
+    SELECT test_bool, AVG(test_float) AS avg, SUM(test_int) AS sum 
+    FROM test_table
+    WHERE test_nullint IS NOT NULL 
+    GROUP BY test_bool 
+    ORDER BY sum
+    """
+    df = read_sql(sqlite_db, query)
+    print(df)
+    expected = pd.DataFrame(
+        index=range(3),
+        data={
+            "test_bool": pd.Series([False, None, True], dtype="boolean"),
+            "avg": pd.Series([3.00, 5.45, -10.00], dtype="float64"),
+            "sum": pd.Series([3, 4, 1315], dtype="Int64"),
+        },
+    )
+    assert_frame_equal(df, expected, check_names=True)
+
+def test_read_sql_with_partition_and_spa(sqlite_db: str) -> None:
+    query = """
+    SELECT test_bool, AVG(test_float) AS avg, SUM(test_int) AS sum 
+    FROM test_table
+    WHERE test_nullint IS NOT NULL 
+    GROUP BY test_bool 
+    ORDER BY sum
+    """
+    df = read_sql(sqlite_db, query, partition_on="sum", partition_num=2)
+    expected = pd.DataFrame(
+        index=range(3),
+        data={
+            "test_bool": pd.Series([False, None, True], dtype="boolean"),
+            "avg": pd.Series([3.00, 5.45, -10.00], dtype="float64"),
+            "sum": pd.Series([3, 4, 1315], dtype="Int64"),
+        },
+    )
+    df = df.sort_values("sum").reset_index(drop=True)
+    assert_frame_equal(df, expected, check_names=True)
+
 
 def test_empty_result(sqlite_db: str) -> None:
     query = "SELECT * FROM test_table where test_int < -100"
@@ -121,7 +209,6 @@ def test_empty_result(sqlite_db: str) -> None:
         }
     )
     assert_frame_equal(df, expected, check_names=True)
-
 
 def test_empty_result_on_partition(sqlite_db: str) -> None:
     query = "SELECT * FROM test_table where test_int < -100"
