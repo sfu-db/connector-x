8beca50e0f2cf5ed8319c2a43acb3e307fd9ad6f
Weiyuan Wu <youngw@sfu.ca>
2021-10-12 19:51:35 +0000

    cherry pick: use Py::from_owned_ptr instead of from_borrowed_ptr

--- connectorx-python/src/pandas/destination.rs
+++ connectorx-python/src/pandas/destination.rs
@@ -95,6 +95,7 @@ impl<'a> PandasDestination<'a> {
             dt,
             cids: placement,
         };
+
         self.block_datas.push(data.into());
         self.block_infos.push(block_info);
     }
