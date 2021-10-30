15afac2aee90ff5d6649ca9963e3233f5f2f52a9
Xiaoying Wang <wangxiaoying0369@gmail.com>
2021-10-30 05:14:40 +0000

    update dask tpch bench script

--- benchmarks/tpch-dask.py
+++ benchmarks/tpch-dask.py
@@ -1,12 +1,18 @@
 """
 Usage:
-  tpch-dask.py <num> [--conn=<conn>] [--index=<idx>]
+  tpch-dask.py <num> [--conn=<conn>] [--index=<idx>] [--driver=<driver>]
 
 Options:
   --conn=<conn>          The connection url to use [default: POSTGRES_URL].
   --index=<idx>          The connection url to use [default: l_orderkey].
+  --driver=<driver>         The driver to use using sqlalchemy: https://docs.sqlalchemy.org/en/14/core/engines.html.
   -h --help     Show this screen.
   --version     Show version.
+
+Drivers:
+  PostgreSQL: postgresql, postgresql+psycopg2
+  MySQL: mysql, mysql+mysqldb, mysql+pymysql
+  Redshift: postgresql, redshift, redshift+psycopg2
 """
 
 import os
@@ -15,20 +21,27 @@ import dask.dataframe as dd
 from contexttimer import Timer
 from docopt import docopt
 from dask.distributed import Client, LocalCluster
+from sqlalchemy.engine.url import make_url
 
 if __name__ == "__main__":
     args = docopt(__doc__, version="Naval Fate 2.0")
     index_col = args["--index"]
     conn = os.environ[args["--conn"]]
+    conn = make_url(conn)
     table = os.environ["TPCH_TABLE"]
+    driver = args.get("--driver", None)
     npartition = int(args["<num>"])
+
     cluster = LocalCluster(n_workers=npartition, scheduler_port=0, memory_limit="230G")
     client = Client(cluster)
 
     # https://docs.sqlalchemy.org/en/13/core/engines.html#sqlite
     # 4 initial slashes is needed for Unix/Mac
-    if conn.startswith("sqlite"):
-      conn = f"sqlite:///{conn[9:]}"
+    if conn.drivername == "sqlite":
+        conn = f"sqlite:///{str(conn)[9:]}"
+    elif driver is not None:
+        conn = str(conn.set(drivername=driver))
+    print(f"conn url: {conn}")
 
     with Timer() as timer:
         df = dd.read_sql_table(
