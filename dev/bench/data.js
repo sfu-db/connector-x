017daa70781cbfac7cea09a677d25c445dcebc9f
github-action-benchmark <github@users.noreply.github.com>
2021-11-21 06:14:06 +0000

    add ConnectorX TPC-H Scale@1 Benchmarks (pytest) benchmark result for 2cb8bb2002a0b47f553c5af14a4eece437d6eaab

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1637176283474,
+  "lastUpdate": 1637475246006,
   "repoUrl": "https://github.com/sfu-db/connector-x",
   "entries": {
     "ConnectorX TPC-H Scale@1 Benchmarks": [
@@ -4902,6 +4902,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 2.1460163147331177",
             "extra": "mean: 17.922536714933813 sec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "wangxiaoying0369@gmail.com",
+            "name": "Xiaoying Wang",
+            "username": "wangxiaoying"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2cb8bb2002a0b47f553c5af14a4eece437d6eaab",
+          "message": "Merge pull request #173 from sfu-db/fix-postgres-bytea\n\nFix postgres to arrow type: bytea",
+          "timestamp": "2021-11-20T22:02:44-08:00",
+          "tree_id": "385381ee35f67fd72aa2c9e346db67ea06557309",
+          "url": "https://github.com/sfu-db/connector-x/commit/2cb8bb2002a0b47f553c5af14a4eece437d6eaab"
+        },
+        "date": 1637475243849,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "connectorx/tests/benchmarks.py::bench_mysql",
+            "value": 0.061855615768619455,
+            "unit": "iter/sec",
+            "range": "stddev: 0.31686096954862664",
+            "extra": "mean: 16.16668086759746 sec\nrounds: 5"
+          },
+          {
+            "name": "connectorx/tests/benchmarks.py::bench_postgres",
+            "value": 0.060677997885182605,
+            "unit": "iter/sec",
+            "range": "stddev: 3.110599947322996",
+            "extra": "mean: 16.480438294820487 sec\nrounds: 5"
           }
         ]
       }
