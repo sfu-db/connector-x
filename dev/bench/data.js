b5c978abb75794db34cee310654ba2143164f6a8
github-action-benchmark <github@users.noreply.github.com>
2022-01-24 06:55:38 +0000

    add ConnectorX TPC-H Scale@1 Benchmarks (pytest) benchmark result for e5af66503442808cca7c3e4c1b792490659e00e0

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1642799636121,
+  "lastUpdate": 1643007338317,
   "repoUrl": "https://github.com/sfu-db/connector-x",
   "entries": {
     "ConnectorX TPC-H Scale@1 Benchmarks": [
@@ -6802,6 +6802,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 1.9550342354303072",
             "extra": "mean: 13.322715456783772 sec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "wangxiaoying0369@gmail.com",
+            "name": "Xiaoying Wang",
+            "username": "wangxiaoying"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e5af66503442808cca7c3e4c1b792490659e00e0",
+          "message": "[skil ci] add technical report",
+          "timestamp": "2022-01-23T22:39:07-08:00",
+          "tree_id": "258683b3f1b2b391ef200311edc22fb7eb7171ce",
+          "url": "https://github.com/sfu-db/connector-x/commit/e5af66503442808cca7c3e4c1b792490659e00e0"
+        },
+        "date": 1643007336882,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "connectorx/tests/benchmarks.py::bench_mysql",
+            "value": 0.045660569425373575,
+            "unit": "iter/sec",
+            "range": "stddev: 0.7754797467976594",
+            "extra": "mean: 21.90073432251811 sec\nrounds: 5"
+          },
+          {
+            "name": "connectorx/tests/benchmarks.py::bench_postgres",
+            "value": 0.051706597380005256,
+            "unit": "iter/sec",
+            "range": "stddev: 1.6848829319839704",
+            "extra": "mean: 19.33989182561636 sec\nrounds: 5"
           }
         ]
       }
