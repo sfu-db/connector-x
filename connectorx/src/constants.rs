32a854ee37814e3e49a55d884ced8b17727b84cc
Xiaoying Wang <xiaoying_wang@sfu.ca>
2022-02-08 18:15:58 +0000

    set oracle array size to 1k

--- connectorx/src/constants.rs
+++ connectorx/src/constants.rs
@@ -1,6 +1,7 @@
 #[cfg(any(feature = "dst_arrow", feature = "dst_arrow2"))]
 pub(crate) const SECONDS_IN_DAY: i64 = 86_400;
 
+#[allow(dead_code)]
 const KILO: usize = 1 << 10;
 
 #[cfg(any(feature = "dst_arrow", feature = "dst_arrow2"))]
@@ -15,4 +16,4 @@ pub const RECORD_BATCH_SIZE: usize = 64 * KILO;
 pub const DB_BUFFER_SIZE: usize = 32;
 
 #[cfg(any(feature = "src_oracle"))]
-pub const ORACLE_ARRAY_SIZE: u32 = 1024;
+pub const ORACLE_ARRAY_SIZE: u32 = (1 * KILO) as u32;
