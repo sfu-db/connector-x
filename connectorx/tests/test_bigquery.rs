bd0ee5158dff84ce9946a52214e968f2a97da04f
Wukkkinz-0725 <yca375@sfu.ca>
2022-04-07 23:36:58 +0000

    add rust-side bigquery test

--- connectorx/tests/test_bigquery.rs
+++ connectorx/tests/test_bigquery.rs
@@ -24,14 +24,13 @@ fn test_source() {
 }
 
 #[test]
-fn test_bigquery_partition_3() {
+fn test_bigquery_partition() {
     let dburl = "bigquery:///home/jinze/dataprep-bigquery-d6514e01c1db.json";
     let rt = Arc::new(Runtime::new().unwrap());
     let mut source = BigQuerySource::new(rt, &dburl).unwrap();
     let queries = [
-        CXQuery::naked("SELECT * FROM (SELECT * FROM `dataprep-bigquery.dataprep.lineitem` LIMIT 1000) AS CXTMPTAB_PART WHERE 19419500 <= CXTMPTAB_PART.L_ORDERKEY AND CXTMPTAB_PART.L_ORDERKEY < 38837719"),
-        CXQuery::naked("SELECT * FROM (SELECT * FROM `dataprep-bigquery.dataprep.lineitem` LIMIT 1000) AS CXTMPTAB_PART WHERE 1281 <= CXTMPTAB_PART.L_ORDERKEY AND CXTMPTAB_PART.L_ORDERKEY < 19419500"),
-        CXQuery::naked("SELECT * FROM (SELECT * FROM `dataprep-bigquery.dataprep.lineitem` LIMIT 1000) AS CXTMPTAB_PART WHERE 38837719 <= CXTMPTAB_PART.L_ORDERKEY AND CXTMPTAB_PART.L_ORDERKEY < 58255940"),
+        CXQuery::naked("SELECT * FROM (SELECT * FROM `dataprep-bigquery.dataprep.lineitem` LIMIT 1000) AS CXTMPTAB_PART WHERE 1281 <= CXTMPTAB_PART.L_ORDERKEY AND CXTMPTAB_PART.L_ORDERKEY < 29128610"),
+        CXQuery::naked("SELECT * FROM (SELECT * FROM `dataprep-bigquery.dataprep.lineitem` LIMIT 1000) AS CXTMPTAB_PART WHERE 29128610 <= CXTMPTAB_PART.L_ORDERKEY AND CXTMPTAB_PART.L_ORDERKEY < 58255940"),
     ];
     let mut destination = ArrowDestination::new();
     let dispatcher = Dispatcher::<_, _, BigQueryArrowTransport>::new(
@@ -42,5 +41,4 @@ fn test_bigquery_partition_3() {
     );
     dispatcher.run().unwrap();
     let result = destination.arrow().unwrap();
-    println!("{:?}", result);
 }
