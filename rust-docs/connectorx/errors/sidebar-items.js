08ee99cba21eceb169a0fb702d40fd3bf0d9c702
wangxiaoying <wangxiaoying0369@gmail.com>
2022-09-13 22:13:29 +0000

    Deploying to gh-pages from @ sfu-db/connector-x@a69f8a388db4a9e9b9b51928aa0c3aa04bd6fd4e 🚀

--- rust-docs/connectorx/errors/sidebar-items.js
+++ rust-docs/connectorx/errors/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["ConnectorXError","Errors that can be raised from this library."],["ProduceContext",""]],"type":[["Result",""]]});
+initSidebarItems({"enum":[["ConnectorXError","Errors that can be raised from this library."],["ConnectorXOutError",""],["ProduceContext",""]],"type":[["OutResult",""],["Result",""]]});
---
+++ rust-docs/connectorx/get_arrow/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["get_arrow",""]]});
