c1e2fb1243d825d1b582d08d39cd27c8aa53bb02
auyer <rafael@rcpassos.me>
2022-11-01 11:30:34 -0300

    Polars uses Arrow2 in their code. So should we
    Polars refrence: https://github.com/pola-rs/polars/blob/9dabbe5b4d0d21a65bf6e2a52062175485603a86/py-polars/polars/io.py#L1096

--- connectorx-python/connectorx/__init__.py
+++ connectorx-python/connectorx/__init__.py
@@ -185,10 +185,10 @@ def read_sql(
                 raise ValueError("You need to install polars first")
 
             try:
-                df = pl.DataFrame.from_arrow(df)
-            except AttributeError:
                 # api change for polars >= 0.8.*
                 df = pl.from_arrow(df)
+            except AttributeError:
+                df = pl.DataFrame.from_arrow(df)
         return df
 
     if isinstance(query, str):
@@ -256,7 +256,7 @@ def read_sql(
 
         result = _read_sql(
             conn,
-            "arrow" if return_type in {"arrow", "polars"} else "arrow2",
+            "arrow2" if return_type in {"arrow2", "polars", "polars2"} else "arrow",
             queries=queries,
             protocol=protocol,
             partition_query=partition_query,
