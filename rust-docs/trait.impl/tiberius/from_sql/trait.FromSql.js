3fbd93af72991b81a31012db03eeb17331f7e1f9
wangxiaoying <wangxiaoying0369@gmail.com>
2024-03-30 16:38:34 +0000

    Deploying to gh-pages from @ sfu-db/connector-x@20130bce58483a54cf5dbd41c10dd94b2a0a5b09 🚀

--- rust-docs/trait.impl/tiberius/from_sql/trait.FromSql.js
+++ rust-docs/trait.impl/tiberius/from_sql/trait.FromSql.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"connectorx":[["impl&lt;'a&gt; FromSql&lt;'a&gt; for <a class=\"struct\" href=\"connectorx/sources/mssql/struct.IntN.html\" title=\"struct connectorx::sources::mssql::IntN\">IntN</a>"],["impl&lt;'a&gt; FromSql&lt;'a&gt; for <a class=\"struct\" href=\"connectorx/sources/mssql/struct.FloatN.html\" title=\"struct connectorx::sources::mssql::FloatN\">FloatN</a>"]]
+"connectorx":[["impl&lt;'a&gt; FromSql&lt;'a&gt; for <a class=\"struct\" href=\"connectorx/sources/mssql/struct.FloatN.html\" title=\"struct connectorx::sources::mssql::FloatN\">FloatN</a>"],["impl&lt;'a&gt; FromSql&lt;'a&gt; for <a class=\"struct\" href=\"connectorx/sources/mssql/struct.IntN.html\" title=\"struct connectorx::sources::mssql::IntN\">IntN</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
