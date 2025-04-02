dfcf128490866a17f7140fcabdb48aaae357d3c2
wangxiaoying <wangxiaoying0369@gmail.com>
2025-04-02 20:38:54 +0000

    Deploying to gh-pages from @ sfu-db/connector-x@d5914514a458fc1cbc1ef464ed01e08cf2b1bf5a 🚀

--- rust-docs/trait.impl/serde/de/trait.DeserializeSeed.js
+++ rust-docs/trait.impl/serde/de/trait.DeserializeSeed.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["connectorx",[["impl&lt;'_a, '_de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.217/serde/de/trait.DeserializeSeed.html\" title=\"trait serde::de::DeserializeSeed\">DeserializeSeed</a>&lt;'_de&gt; for <a class=\"struct\" href=\"connectorx/sources/trino/struct.__TrinoPartitionQueryResultSeed.html\" title=\"struct connectorx::sources::trino::__TrinoPartitionQueryResultSeed\">__TrinoPartitionQueryResultSeed</a>&lt;'_a&gt;"]]]]);
+    var implementors = Object.fromEntries([["connectorx",[["impl&lt;'_a, '_de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.219/serde/de/trait.DeserializeSeed.html\" title=\"trait serde::de::DeserializeSeed\">DeserializeSeed</a>&lt;'_de&gt; for <a class=\"struct\" href=\"connectorx/sources/trino/struct.__TrinoPartitionQueryResultSeed.html\" title=\"struct connectorx::sources::trino::__TrinoPartitionQueryResultSeed\">__TrinoPartitionQueryResultSeed</a>&lt;'_a&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
