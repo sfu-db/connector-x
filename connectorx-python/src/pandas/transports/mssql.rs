37dd8ed360c54cd6066f1d16fb6b136b80d6992c
Xiaoying Wang <xiaoying_wang@sfu.ca>
2025-04-02 13:30:41 -0700

    0.4.3-alpha.1: attempt to fix mssql bb8 timeout by downgrading tiberius and bb8

--- connectorx-python/src/pandas/transports/mssql.rs
+++ connectorx-python/src/pandas/transports/mssql.rs
@@ -7,7 +7,7 @@ use connectorx::{
     typesystem::TypeConversion,
 };
 use rust_decimal::prelude::*;
-use uuid::Uuid;
+use uuid_old::Uuid;
 
 #[allow(dead_code)]
 pub struct MsSQLPandasTransport<'py>(&'py ());
