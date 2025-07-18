d6beed782887dc6bd0586122ac79779ca98b5b0f
Devin Christensen <quixoten@gmail.com>
2025-07-17 18:40:16 -0600

    try an additional format to parse bq timestamps

--- connectorx/src/sources/bigquery/mod.rs
+++ connectorx/src/sources/bigquery/mod.rs
@@ -768,6 +768,7 @@ impl<'r, 'a> Produce<'r, NaiveDateTime> for BigQuerySourceParser {
             .as_str()
             .ok_or_else(|| anyhow!("cannot get str from json value"))?;
         NaiveDateTime::parse_from_str(s, "%Y-%m-%dT%H:%M:%S")
+            .or_else(|_| NaiveDateTime::parse_from_str(s, "%Y-%m-%dT%H:%M:%S%.F"))
             .map_err(|_| ConnectorXError::cannot_produce::<NaiveDateTime>(Some(s.into())))?
     }
 }
@@ -838,7 +839,9 @@ impl<'r, 'a> Produce<'r, Option<NaiveDateTime>> for BigQuerySourceParser {
                     .as_str()
                     .ok_or_else(|| anyhow!("cannot get str from json value"))?;
                 Some(
-                    NaiveDateTime::parse_from_str(s, "%Y-%m-%dT%H:%M:%S").map_err(|_| {
+                    NaiveDateTime::parse_from_str(s, "%Y-%m-%dT%H:%M:%S")
+                    .or_else(|_| NaiveDateTime::parse_from_str(s, "%Y-%m-%dT%H:%M:%S%.F"))
+                    .map_err(|_| {
                         ConnectorXError::cannot_produce::<NaiveDateTime>(Some(s.into()))
                     })?,
                 )
