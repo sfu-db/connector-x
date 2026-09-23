f4f1f426e72228bc5dc968a1e3abf05b61678308
saurabh500 <saurabh.singh@microsoft.com>
2026-09-23 01:38:39 +0000

    Deploying to gh-pages from @ sfu-db/connector-x@7df03b5398911e03cecad31ea748f08cd4e4c00b 🚀

---
+++ rust-docs/trait.impl/serde_core/de/trait.DeserializeSeed.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["connectorx",[["impl&lt;'_a, '_de&gt; <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.229/serde_core/de/trait.DeserializeSeed.html\" title=\"trait serde_core::de::DeserializeSeed\">DeserializeSeed</a>&lt;'_de&gt; for <a class=\"struct\" href=\"connectorx/sources/trino/struct.__TrinoPartitionQueryResultSeed.html\" title=\"struct connectorx::sources::trino::__TrinoPartitionQueryResultSeed\">__TrinoPartitionQueryResultSeed</a>&lt;'_a&gt;",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[454]}
